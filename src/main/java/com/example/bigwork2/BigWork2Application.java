ad1539fd89fe901c2a1dafe0e7e4c9358166cc55
V-I-K-I <anystrelchenko@gmail.com>
2022-08-16 22:26:40 +0300

    Initial commit

---
+++ src/main/java/com/example/bigwork2/BigWork2Application.java
@@ -0,0 +1,13 @@
+package com.example.bigwork2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigWork2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigWork2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bigwork2/BigWork2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bigwork2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigWork2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
